376ebefa8874a718d19e64910f2a216f9208e67f
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2018-09-28 19:39:49 +0200

    lesson12 if statements

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert("Hello, world!");
+var youLikeMeat = false;
+var myNum = 10;
+
+if(!youLikeMeat) {
+    document.write("Here is the veggie menu...");
+} else {
+    document.write("Here is the meat menu...");
+}
